99a95ed539b0f6b0af3cb89f00150594a878ee4e
erich brunner <erich.brunner@live.at>
2016-03-13 12:28:15 +0100

    added StringExtensions Assembly DLL

---
+++ NuGetPackageSolution/StringExtensions.NuGet/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtensions.NuGet
+{
+    public static class StringExtensions
+    {
+        public static string PrintMeToConsole(this string text)
+        {
+            Console.WriteLine(text);
+            return text;
+        }
+    }
+}
